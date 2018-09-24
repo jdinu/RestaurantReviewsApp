5e26cd84b99c23e8002385fb7c17774dfcbc9933
Jaya Dinu <jayadinu@gmail.com>
2018-09-24 18:36:01 -0400

    map api connected

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'restreviewapp',
+        mapboxToken: 'pk.eyJ1IjoiamF5YWRpbnUiLCJhIjoiY2ptZG44NGo2MHIzaDNwcXQ5MWF3eTVhdCJ9.GYgdOgeeY0Be1Vy2VY9F2g',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
